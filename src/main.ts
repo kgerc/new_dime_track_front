d7385c18fba4cf986fcbb1ce266ab6202259a4c1
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2025-02-16 18:50:33 +0100

    added new front project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import App from './App.vue'
+import Button from "primevue/button"
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.component('Button', Button);
+app.mount('#app');
