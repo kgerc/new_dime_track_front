f172f5a244a59a2a459d70cc3392e5dfd89700df
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2025-03-08 08:08:52 +0100

    refreshed token

--- src/stores/commonStore.js
+++ src/stores/commonStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useCommonStore = defineStore('common', () => {
-  const token = ref('eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImtyenlzIiwibmFtZWlkIjoiMmJiMjE5ZDUtYjkzZi00YmQ0LTljNmItZjRhNjlmNTEzY2Y1IiwiZW1haWwiOiJrcnp5c0B0ZXN0LmNvbSIsIm5iZiI6MTc0MDc3Mjg5MiwiZXhwIjoxNzQxMzc3NjkyLCJpYXQiOjE3NDA3NzI4OTJ9.wVAgk36BIcwO_4gNx4TdqJDnhjwpSrM1z-sDJcP3U4C4eaXZo0hscmzftoqwDC3S51vssgMeFxUoiXwlK459zQ')
+  const token = ref('eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImtyenlzIiwibmFtZWlkIjoiMmJiMjE5ZDUtYjkzZi00YmQ0LTljNmItZjRhNjlmNTEzY2Y1IiwiZW1haWwiOiJrcnp5c0B0ZXN0LmNvbSIsIm5iZiI6MTc0MTQxNzYxMSwiZXhwIjoxNzQyMDIyNDExLCJpYXQiOjE3NDE0MTc2MTF9.cWjqhaHypgxMCzQwegcY7TT_tkt73OeK75or5MThYx1Ipp0Tj_c1KI9AclmkHussS4WfXvpN9hWYHb4MdLl1Ng')
 
   function setToken(newToken) {
     token.value = newToken
